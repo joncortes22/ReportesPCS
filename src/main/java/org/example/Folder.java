a6cf97e88fb86b4af7f5a1f695aff75d7bf99995
JonathanCortes <joncortes2212@gmail.com>
2024-06-30 00:54:02 -0600

    Initial commit

---
+++ src/main/java/org/example/Folder.java
@@ -0,0 +1,75 @@
+package org.example;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.*;
+import java.util.HashMap;
+
+public class Folder extends Thread{
+    private File path;
+
+    public Folder(File path) {
+        this.path = path;
+    }
+
+    public void getFileInfo(File file) throws FileNotFoundException {
+        Scanner reader = new Scanner(file);
+
+        String pattern = "reporte_(\\d{2}_\\d{2}_\\d{4})\\.txt";
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(file.getName());
+        String fecha = "";
+        matcher.matches();
+        if (matcher.matches()){fecha = matcher.group(1);}
+        fecha = fecha.replace("_", "-");
+
+        String nombre = "";
+        String[] hallazgos = new String[3];
+        while (reader.hasNextLine()){
+            String data = reader.nextLine();
+            if (data.startsWith("# Topico del Reporte")){
+                nombre = reader.nextLine();
+                nombre = nombre.replace("- ", "");
+            } else if (data.startsWith("# Hallazgos")) {
+                hallazgos[0] = reader.nextLine();
+                hallazgos[1] = reader.nextLine();
+                hallazgos[2] = reader.nextLine();
+
+                HashMap<String, String> newReport = new HashMap<>();
+                newReport.put("fecha", fecha);
+                newReport.put("nombre", nombre);
+                newReport.put("hallazgos", Arrays.toString(hallazgos));
+                Main.reportHashMap.add(newReport);
+                return;
+            }
+        }
+    }
+
+    public void checkFolder(File path) throws FileNotFoundException {
+        File[] fileList = path.listFiles();
+        assert fileList != null;
+        String pattern = "reporte_\\d{2}_\\d{2}_\\d{4}.txt";
+        Pattern compiledPattern = Pattern.compile(pattern);
+
+        for (File file : fileList) {
+            if (file.isDirectory()) {
+                checkFolder(file);
+            } else {
+                Matcher matcher = compiledPattern.matcher(file.getName());
+                if (matcher.matches()){
+                    getFileInfo(file);
+                }
+            }
+        }
+
+    }
+    @Override
+    public void run() {
+        try {
+            checkFolder(this.path);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,86 @@
+package org.example;
+import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Date;
+
+
+public class Main {
+    public static List<HashMap<String, String>> reportHashMap = Collections.synchronizedList(new ArrayList<>());
+
+    public static void ordenarReportes() throws ParseException {
+        for (int i = 0; i<=reportHashMap.size()-1;i++){
+            for (int x = 0; x <= reportHashMap.size()-1;x++){
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+                Date fecha1 = formatter.parse((String) reportHashMap.get(i).get("fecha"));
+                Date fecha2 = formatter.parse((String) reportHashMap.get(x).get("fecha"));
+                if (fecha2.compareTo(fecha1) > 0){
+                    HashMap<String, String> temp = reportHashMap.get(i);
+                    reportHashMap.set(i, reportHashMap.get(x));
+                    reportHashMap.set(x, temp);
+                }
+            }
+
+        }
+    }
+
+    public static void generarReporteFinal() throws IOException, ParseException {
+        File reporteFinal = new File("reporteFinal.txt");
+        reporteFinal.createNewFile();
+
+        StringBuilder dataReporte = new StringBuilder();
+        String currentMonth = "";
+        for (HashMap reporte : reportHashMap){
+            String fecha = (String) reporte.get("fecha");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.forLanguageTag("es"));
+
+            Date date = dateFormat.parse(fecha);
+            SimpleDateFormat formatoMes = new SimpleDateFormat("MMMM yyyy", Locale.forLanguageTag("es"));
+            String mes = formatoMes.format(date);
+            if (!mes.equals(currentMonth)){
+                currentMonth = mes;
+                dataReporte.append(currentMonth).append("\n");
+            }
+
+            dataReporte.append("# Reporte del ").append(reporte.get("fecha")).append("\n");
+            dataReporte.append("# ").append(reporte.get("nombre")).append("\n");
+            String strHallazgos = (String) reporte.get("hallazgos");
+            String[] hallazgos = strHallazgos.split(",");
+            for (String hallazgo : hallazgos){
+                hallazgo = hallazgo.replace("[", " ");
+                hallazgo = hallazgo.replace("]", "");
+                dataReporte.append(hallazgo).append("\n");
+            }
+            dataReporte.append("\n");
+        }
+        FileWriter writer = new FileWriter(reporteFinal);
+        writer.write(String.valueOf(dataReporte));
+        writer.close();
+    }
+
+    public static void main(String[] args) throws InterruptedException, ParseException, IOException {
+        File mainFolder = new File("C:\\Users\\jonco\\Documents\\GIT Projects\\ReportGenerator\\src\\main\\java\\org\\example\\bitacora");
+        File[] fileList = mainFolder.listFiles();
+        assert fileList != null;
+
+        int folderCount = fileList.length;
+        Folder[] folderThreads = new Folder[folderCount];
+        int counter = 0;
+        for (File file:fileList){
+            folderThreads[counter] = new Folder(file);
+            folderThreads[counter].start();
+            counter++;
+        }
+        for (Folder folder : folderThreads){
+            folder.join();
+        }
+        ordenarReportes();
+        generarReporteFinal();
+
+    }
+}
